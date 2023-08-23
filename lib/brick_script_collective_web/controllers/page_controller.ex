efb9d6e5798ae716e5bd40188bc11fe436ae91ef
Erik Mejer Hansen <erik.m.hansen@maersk.com>
2023-08-23 20:45:35 +0200

    :coffin: delete controller code

--- lib/brick_script_collective_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule BrickScriptCollectiveWeb.PageController do
-  use BrickScriptCollectiveWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
--- lib/brick_script_collective_web/controllers/page_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule BrickScriptCollectiveWeb.PageHTML do
-  use BrickScriptCollectiveWeb, :html
-
-  embed_templates "page_html/*"
-end
